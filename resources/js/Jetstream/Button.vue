ea31a0f59aa6c3aa0d941d11087372a9b3e7e62d
jeffreybalmes <jefbalmes@gmail.com>
2020-12-12 08:45:46 +0800

    Laravel Installation

---
+++ resources/js/Jetstream/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="col-span-6 sm:col-span-4 mb-5">
+        <button :type="type" class="inline-flex items-center px-4 py-2 bg-blue-900 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+            <slot></slot>
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            type: {
+                type: String,
+                default: 'submit',
+            },
+        }
+    }
+</script>
---
+++ resources/js/Jetstream/Input.vue
@@ -0,0 +1,18 @@
+<template>
+	 <div class="col-span-6 sm:col-span-4 mb-5">
+    	<input class="form-input rounded-md shadow-sm" :value="value" @input="$emit('input', $event.target.value)" ref="input">
+ 	 </div>
+</template>
+
+<script>
+    export default {
+        props: ['value'],
+
+        methods: {
+            focus() {
+                this.$refs.input.focus()
+            }
+        }
+    }
+</script>
+
