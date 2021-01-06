027b2b4e3f4f31fd83bec631724aa6aade6d0114
jeffreybalmes <jefbalmes@gmail.com>
2021-01-06 14:13:47 +0800

    Inertiajs registerLengthAwarePaginator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->registerGlide();
+        $this->registerLengthAwarePaginator();
+
         Inertia::share('app.name', config('app.name'));
 
         Inertia::share('errors', function() {
@@ -38,6 +41,91 @@ class AppServiceProvider extends ServiceProvider
             ];
         });
     }
+
+    protected function registerGlide()
+    {
+        $this->app->bind(Server::class, function ($app) {
+            return Server::create([
+                'source' => Storage::getDriver(),
+                'cache' => Storage::getDriver(),
+                'cache_folder' => '.glide-cache',
+                'base_url' => 'img',
+            ]);
+        });
+    }
+
+    protected function registerLengthAwarePaginator()
+    {
+        $this->app->bind(LengthAwarePaginator::class, function ($app, $values) {
+            return new class(...array_values($values)) extends LengthAwarePaginator {
+                public function only(...$attributes)
+                {
+                    return $this->transform(function ($item) use ($attributes) {
+                        return $item->only($attributes);
+                    });
+                }
+
+                public function transform($callback)
+                {
+                    $this->items->transform($callback);
+
+                    return $this;
+                }
+
+                public function toArray()
+                {
+                    return [
+                        'data' => $this->items->toArray(),
+                        'links' => $this->links(),
+                    ];
+                }
+
+                public function links($view = null, $data = [])
+                {
+                    $this->appends(Request::all());
+
+                    $window = UrlWindow::make($this);
+
+                    $elements = array_filter([
+                        $window['first'],
+                        is_array($window['slider']) ? '...' : null,
+                        $window['slider'],
+                        is_array($window['last']) ? '...' : null,
+                        $window['last'],
+                    ]);
+
+                    return Collection::make($elements)->flatMap(function ($item) {
+                        if (is_array($item)) {
+                            return Collection::make($item)->map(function ($url, $page) {
+                                return [
+                                    'url' => $url,
+                                    'label' => $page,
+                                    'active' => $this->currentPage() === $page,
+                                ];
+                            });
+                        } else {
+                            return [
+                                [
+                                    'url' => null,
+                                    'label' => '...',
+                                    'active' => false,
+                                ],
+                            ];
+                        }
+                    })->prepend([
+                        'url' => $this->previousPageUrl(),
+                        'label' => 'Previous',
+                        'active' => false,
+                    ])->push([
+                        'url' => $this->nextPageUrl(),
+                        'label' => 'Next',
+                        'active' => false,
+                    ]);
+                }
+            };
+        });
+    }
+
 
     /**
      * Bootstrap any application services.
