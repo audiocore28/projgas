08e2e9516d0c172d62e3f7af6a01161f5e699df4
jeffreybalmes <jefbalmes@gmail.com>
2021-02-15 14:48:45 +0800

    added routes & re-layout main menu

--- resources/js/Shared/MainMenu.vue
+++ resources/js/Shared/MainMenu.vue
@@ -1,10 +1,58 @@
 <template>
   <div>
-    <div class="mb-4" v-for="link in links">
-      <inertia-link class="flex items-center group py-3" :href="route(link.route)">
-        <icon :name="link.icon" class="w-4 h-4 mr-2" :class="isUrl(link.isUrl) ? 'fill-white' : 'fill-blue-400 group-hover:fill-white'" />
-        <div :class="isUrl(link.isUrl) ? 'text-white' : 'text-blue-300 group-hover:text-white'">{{ link.name }}</div>
-      </inertia-link>
+    <h4 @click="truckingSelected !== 0 ? truckingSelected = 0 : truckingSelected = null"
+    class="text-blue-300 text-xs cursor-pointer py-3 hover:text-white opacity-75 rounded-t">Trucking</h4>
+    <div v-show="truckingSelected == 0" class="py-4 text-sm">
+      <div class="mb-4" v-for="link in truckingLinks">
+        <inertia-link class="flex items-center group py-3" :href="route(link.route)">
+          <icon :name="link.icon" class="w-4 h-4 mr-2" :class="isUrl(link.isUrl) ? 'fill-white' : 'fill-blue-400 group-hover:fill-white'" />
+          <div :class="isUrl(link.isUrl) ? 'text-white' : 'text-blue-300 group-hover:text-white'">{{ link.name }}</div>
+        </inertia-link>
+      </div>
+    </div>
+
+    <h4 @click="recordSelected !== 1 ? recordSelected = 1 : recordSelected = null"
+    class="text-blue-300 text-xs cursor-pointer py-3 hover:text-white opacity-75 rounded-t">Records</h4>
+    <div v-show="recordSelected == 1" class="py-4 text-sm">
+      <div class="mb-4" v-for="link in recordLinks">
+        <inertia-link class="flex items-center group py-3" :href="route(link.route)">
+          <icon :name="link.icon" class="w-4 h-4 mr-2" :class="isUrl(link.isUrl) ? 'fill-white' : 'fill-blue-400 group-hover:fill-white'" />
+          <div :class="isUrl(link.isUrl) ? 'text-white' : 'text-blue-300 group-hover:text-white'">{{ link.name }}</div>
+        </inertia-link>
+      </div>
+    </div>
+
+    <h4 @click="reportSelected !== 2 ? reportSelected = 2 : reportSelected = null"
+    class="text-blue-300 text-xs cursor-pointer py-3 hover:text-white opacity-75 rounded-t">Reports</h4>
+    <div v-show="reportSelected == 2" class="py-4 text-sm">
+      <div class="mb-4" v-for="link in reportLinks">
+        <inertia-link class="flex items-center group py-3" :href="route(link.route)">
+          <icon :name="link.icon" class="w-4 h-4 mr-2" :class="isUrl(link.isUrl) ? 'fill-white' : 'fill-blue-400 group-hover:fill-white'" />
+          <div :class="isUrl(link.isUrl) ? 'text-white' : 'text-blue-300 group-hover:text-white'">{{ link.name }}</div>
+        </inertia-link>
+      </div>
+    </div>
+
+    <h4 @click="employeeSelected !== 3 ? employeeSelected = 3 : employeeSelected = null"
+    class="text-blue-300 text-xs cursor-pointer py-3 hover:text-white opacity-75 rounded-t">Employees</h4>
+    <div v-show="employeeSelected == 3" class="py-4 text-sm">
+      <div class="mb-4" v-for="link in employeeLinks">
+        <inertia-link class="flex items-center group py-3" :href="route(link.route)">
+          <icon :name="link.icon" class="w-4 h-4 mr-2" :class="isUrl(link.isUrl) ? 'fill-white' : 'fill-blue-400 group-hover:fill-white'" />
+          <div :class="isUrl(link.isUrl) ? 'text-white' : 'text-blue-300 group-hover:text-white'">{{ link.name }}</div>
+        </inertia-link>
+      </div>
+    </div>
+
+    <h4 @click="assetSelected !== 4 ? assetSelected = 4 : assetSelected = null"
+    class="text-blue-300 text-xs cursor-pointer py-3 hover:text-white opacity-75 rounded-t">Assets</h4>
+    <div v-show="assetSelected == 4" class="py-4 text-sm">
+      <div class="mb-4" v-for="link in assetLinks">
+        <inertia-link class="flex items-center group py-3" :href="route(link.route)">
+          <icon :name="link.icon" class="w-4 h-4 mr-2" :class="isUrl(link.isUrl) ? 'fill-white' : 'fill-blue-400 group-hover:fill-white'" />
+          <div :class="isUrl(link.isUrl) ? 'text-white' : 'text-blue-300 group-hover:text-white'">{{ link.name }}</div>
+        </inertia-link>
+      </div>
     </div>
   </div>
 </template>
@@ -21,20 +69,39 @@ export default {
   },
   data() {
     return {
-      links: [
+      truckingLinks: [
         // { route: 'dashboard', icon: 'office', isUrl: 'dashboard', name: 'Dashboard'},
         { route: 'purchases.index', icon: 'shopping-cart', isUrl: 'purchases', name: 'Purchases'},
         { route: 'tanker-loads.index', icon: 'dashboard', isUrl: 'tanker-loads', name: 'Loads'},
         { route: 'hauls.index', icon: 'location', isUrl: 'hauls', name: 'Hauling'},
         { route: 'deliveries.index', icon: 'location', isUrl: 'deliveries', name: 'Deliveries'},
-        { route: 'suppliers.index', icon: 'users', isUrl: 'suppliers', name: 'Suppliers'},
-        { route: 'clients.index', icon: 'users', isUrl: 'clients', name: 'Clients'},
         // { route: 'statements.index', icon: 'book', isUrl: 'statements', name: 'SOA'},
-        { route: 'products.index', icon: 'store-front', isUrl: 'products', name: 'Products'},
-        { route: 'tankers.index', icon: 'dashboard', isUrl: 'tankers', name: 'Tankers'},
+      ],
+      recordLinks: [
+        { route: 'suppliers.index', icon: 'office', isUrl: 'suppliers', name: 'Suppliers'},
+        { route: 'clients.index', icon: 'office', isUrl: 'clients', name: 'Clients'},
+      ],
+      reportLinks: [
+        { route: 'station-transactions.index', icon: 'store-front', isUrl: 'station-transactions', name: 'DTR'},
+      ],
+      employeeLinks: [
         { route: 'drivers.index', icon: 'users', isUrl: 'drivers', name: 'Drivers'},
         { route: 'helpers.index', icon: 'users', isUrl: 'helpers', name: 'Helpers'},
+        { route: 'cashiers.index', icon: 'users', isUrl: 'cashiers', name: 'Cashiers'},
+        { route: 'pump-attendants.index', icon: 'users', isUrl: 'pump-attendants', name: 'Pump Attnd'},
+        { route: 'office-staffs.index', icon: 'users', isUrl: 'office-staffs', name: 'Office Staffs'},
       ],
+      assetLinks: [
+        { route: 'tankers.index', icon: 'dashboard', isUrl: 'tankers', name: 'Tankers'},
+        { route: 'stations.index', icon: 'store-front', isUrl: 'stations', name: 'G. Stations'},
+        { route: 'companies.index', icon: 'office', isUrl: 'companies', name: 'B. Company'},
+        { route: 'products.index', icon: 'store-front', isUrl: 'products', name: 'Products'},
+      ],
+      truckingSelected: 0,
+      recordSelected: 1,
+      reportSelected: 2,
+      employeeSelected: 3,
+      assetSelected: 4,
     }
   },
   methods: {
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
-use App\Http\Controllers\DashboardController;
+// use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DeliveryController;
 use App\Http\Controllers\DeliveryDetailController;
 use App\Http\Controllers\DriverController;
 use App\Http\Controllers\HaulController;
 use App\Http\Controllers\HaulDetailController;
 use App\Http\Controllers\HelperController;
+use App\Http\Controllers\CashierController;
+use App\Http\Controllers\PumpAttendantController;
+use App\Http\Controllers\OfficeStaffController;
 use App\Http\Controllers\TankerLoadController;
 use App\Http\Controllers\TankerLoadDetailController;
 use App\Http\Controllers\LoginController;
@@ -17,6 +20,10 @@ use App\Http\Controllers\PurchaseController;
 use App\Http\Controllers\PurchaseDetailController;
 // use App\Http\Controllers\StatementController;
 use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\StationController;
+use App\Http\Controllers\PumpController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\StationTransactionController;
 use App\Http\Controllers\TankerController;
 use App\Http\Controllers\UsersController;
 
@@ -51,7 +58,7 @@ use App\Http\Controllers\UsersController;
 
 Route::group(['middleware' => 'auth'], function() {
 	// Dashboard
-	Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+	// Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 	// Purchases
 	Route::resource('purchases', PurchaseController::class);
@@ -81,6 +88,18 @@ Route::group(['middleware' => 'auth'], function() {
 	Route::put('suppliers/{supplier}/restore', [SupplierController::class, 'restore'])->name('suppliers.restore');
 	Route::resource('suppliers', SupplierController::class);
 
+	// Stations
+	Route::put('stations/{station}/restore', [StationController::class, 'restore'])->name('stations.restore');
+	Route::resource('stations', StationController::class);
+
+	// Pumps
+	Route::put('pumps/{pump}/restore', [PumpController::class, 'restore'])->name('pumps.restore');
+	Route::resource('pumps', PumpController::class)->only(['store', 'destroy']);
+
+	// Companies
+	Route::put('companies/{company}/restore', [CompanyController::class, 'restore'])->name('companies.restore');
+	Route::resource('companies', CompanyController::class);
+
 	// Clients
 	Route::put('clients/{client}/restore', [ClientController::class, 'restore'])->name('clients.restore');
 	Route::resource('clients', ClientController::class);
@@ -108,6 +127,23 @@ Route::group(['middleware' => 'auth'], function() {
 	// Helpers
 	Route::put('helpers/{helper}/restore', [HelperController::class, 'restore'])->name('helpers.restore');
 	Route::resource('helpers', HelperController::class);
+
+	// Cashiers
+	Route::put('cashiers/{cashier}/restore', [CashierController::class, 'restore'])->name('cashiers.restore');
+	Route::resource('cashiers', CashierController::class);
+
+	// Pump Attendants
+	Route::put('pump-attendants/{pumpAttendant}/restore', [PumpAttendantController::class, 'restore'])->name('pump-attendants.restore');
+	Route::resource('pump-attendants', PumpAttendantController::class);
+
+	// Office Staffs
+	Route::put('office-staffs/{officeStaff}/restore', [OfficeStaffController::class, 'restore'])->name('office-staffs.restore');
+	Route::resource('office-staffs', OfficeStaffController::class);
+
+	// Station Transactions
+	Route::put('station-transactions/{station-transaction}/restore', [StationTransactionController::class, 'restore'])->name('station-transactions.restore');
+	Route::resource('station-transactions', StationTransactionController::class);
+
 });
 
 
