ab87477dafeae744d48c07453d7cea48719a1618
jeffreybalmes <jefbalmes@gmail.com>
2021-04-05 09:15:05 +0800

    UI - added plus & close icons | SelectInput required sign * | adjust layout paddings

--- resources/js/Shared/SelectInput.vue
+++ resources/js/Shared/SelectInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <label v-if="label" class="form-label" :for="id">{{ label }}:</label>
+    <label v-if="label" class="form-label" :for="id">{{ label }}:<span class="text-red-500">*</span></label>
     <select :id="id" ref="input" v-model="selected" v-bind="$attrs" class="form-select" :class="{ error: error }">
       <slot />
     </select>
