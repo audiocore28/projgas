b9cf76b7c19776c26e66979ecae71abded51f5a0
jeffreybalmes <jefbalmes@gmail.com>
2021-05-11 16:06:11 +0800

    MonthlyBatangasTransaction - re-layout create vue & removed unused codes

--- resources/js/Pages/MonthlyBatangasTransactions/Create.vue
+++ resources/js/Pages/MonthlyBatangasTransactions/Create.vue
@@ -4,141 +4,97 @@
       <inertia-link class="text-blue-600 hover:text-blue-800" :href="route('monthly-batangas-transactions.index')">Batangas Transaction</inertia-link>
       <span class="text-blue-600 font-medium">/</span> {{ `${form.date.month}, ${form.date.year}`}}
     </h1>
-    <div class="bg-white rounded shadow overflow-hidden max-w-6xl">
-      <form @submit.prevent="submit">
+    <form @submit.prevent="submit">
+      <div class="w-full mt-8 px-4">
+        <div class="w-full flex flex-wrap justify-between">
+          <div class="pr-6 pb-4 lg:w-1/2">
 
-        <!-- Transaction -->
-        <div class="-mr-6 -mb-12 flex flex-wrap w-full mt-8 px-8">
-          <div class="w-full flex flex-wrap justify-between">
-            <div class="pr-6 pb-4 lg:w-1/2">
-<!--               <label class="form-label block">Purchase No.:<span class="text-red-500">*</span></label>
-              <multiselect
-                id="purchase_id"
-                name="purchases[]"
-                v-model="form.selectedPurchases"
-                placeholder=""
-                class="mt-3 text-xs"
-                :options="purchases"
-                label="purchase_no"
-                track-by="id"
-                @search-change="onSearchPurchaseChange"
-                @input="onSelectedPurchase"
-                :show-labels="false"
-                :multiple="true"
-              ></multiselect>
- -->
-              <!-- :allow-empty="false" -->
+            <!-- <month-picker @change="showDate"></month-picker> -->
+            <label class="form-label block">Month:<span class="text-red-500">*</span></label>
+            <month-picker-input @input="showDate" :no-default="true"></month-picker-input>
 
-              <!-- <month-picker @change="showDate"></month-picker> -->
-              <label class="form-label block">Month:<span class="text-red-500">*</span></label>
-              <month-picker-input @input="showDate" :no-default="true"></month-picker-input>
-
-            </div>
-<!--             <div class="lg:w-1/3">
-              <label class="form-label block mr-5">Date:<span class="text-red-500">*</span></label>
-              <div class="pr-6 pb-4 mt-3">
-                <date-picker v-model="form.date" :error="errors.date" lang="en" value-type="format" :formatter="momentFormat"></date-picker>
-                <div v-if="errors.date" class="form-error">{{ errors.date }}</div>
-              </div>
-            </div>
- -->
           </div>
-
-<!--           <div class="w-full flex flex-wrap bg-yellow-500 pl-6 pt-4 rounded mb-2 highlight-yellow">
-          </div>
- -->
         </div>
-        <!-- /Transaction -->
+      </div>
 
-        <!-- Details table input form -->
-        <div class="bg-white rounded overflow-x-auto mb-8 mt-4 px-8 flex flex-wrap w-full">
-          <div class="mb-12 overflow-x-auto lg:w-4/6">
-            <table class="min-w-full mt-8 shadow divide-y divide-gray-200">
-              <colgroup>
-                <col span="1" style="width: 15%;">
-                <col span="1" style="width: 30%;">
-                <col span="1" style="width: 30%;">
-                <col span="1" style="width: 25%;">
-              </colgroup>
-              <thead class="bg-gray-50">
-                <tr>
-                  <th scope="col" class="text-center px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">
-                    Trip#<span class="text-red-500">*</span>
-                  </th>
-                  <th scope="col" class="text-center px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">
-                    Driver<span class="text-red-500">*</span>
-                  </th>
-                  <th scope="col" class="text-center px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">
-                    Helper<span class="text-red-500">*</span>
-                  </th>
-                  <th scope="col" class="text-center px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">
-                    Tanker<span class="text-red-500">*</span>
-                  </th>
-                  <th>
-                    <div class="text-center px-6 py-3 whitespace-nowrap text-left text-xs font-medium text-gray-700 uppercase bg-gray">
-                      <button @click.prevent="addNewDetailForm()">
-                        <icon name="plus" class="w-4 h-4 mr-2 fill-green-600"/>
-                      </button>
-                    </div>
-                  </th>
-                </tr>
-              </thead>
-              <tbody class="bg-white divide-y divide-gray-200">
-                <tr v-for="(transaction, index) in form.transactions" :key="index">
-                  <td>
-                    <div class="text-sm font-medium text-gray-900">
-                      <text-input v-model="transaction.trip_no" :error="errors.trip_no"/>
-                    </div>
-                  </td>
-                  <td>
-                    <div class="text-sm font-medium text-gray-900">
-                      <select @change="onChange($event)" :id="`driver-${index}`" v-model="transaction.driver_id" class="form-select" :class="{ error: errors[`driver_id.${index}`] }">
-                        <option :value="null" />
-                        <option v-for="driver in drivers" :key="driver.id" :value="driver.id">{{ driver.name }}</option>
-                      </select>
-                    </div>
-                  </td>
-                  <td>
-                    <div class="text-sm font-medium text-gray-900">
-                      <select :id="`helper-${index}`" v-model="transaction.helper_id" class="form-select" :class="{ error: errors[`helper_id.${index}`] }">
-                        <option :value="null" />
-                        <option v-for="helper in helpers" :key="helper.id" :value="helper.id">{{ helper.name }}</option>
-                      </select>
-                    </div>
-                  </td>
-                  <td class="text-sm text-gray-500">
-                    <div class="text-sm font-medium text-gray-900">
-                      <select :id="`tanker-${index}`" v-model="transaction.tanker_id" class="form-select" :class="{ error: errors[`tanker_id.${index}`] }">
-                        <option :value="null" />
-                        <option v-for="tanker in tankers" :key="tanker.id" :value="tanker.id">{{ tanker.plate_no }}</option>
-                      </select>
-                    </div>
-                  </td>
-                  <td class="text-sm text-gray-500">
-                    <div class=" px-5 text-sm font-medium text-gray-900">
-                    <!-- <div class="px-5 py-4 whitespace-nowrap text-left text-xs font-medium text-gray-500 uppercase bg-white"> -->
-                      <button @click.prevent="deleteDetailForm(index)" type="button" class="bg-white py-1 px-1 flex-shrink-0 text-sm leading-none" tabindex="-1">
-                        <icon name="trash" class="w-4 h-4 mr-2 fill-red-600"/>
-                      </button>
-                    </div>
-                  </td>
-                </tr>
+      <!-- Transactions table input form -->
+      <div class="bg-white rounded shadow overflow-x-auto">
+        <table width="100%" class="w-full whitespace-nowrap shadow divide-y divide-gray-200" cellpadding="5">
+          <colgroup>
+            <col span="1" style="width: 10%;">
+            <col span="1" style="width: 20%;">
+            <col span="1" style="width: 20%;">
+            <col span="1" style="width: 20%;">
+            <col span="1" style="width: 20%;">
+            <col span="1" style="width: 10%;">
+          </colgroup>
+          <thead>
+            <tr class="text-left font-bold">
+              <th align="center" class="px-6 pt-6 pb-4">Trip #</th>
+              <th align="center" class="px-6 pt-6 pb-4">Driver</th>
+              <th align="center" class="px-6 pt-6 pb-4">Helper</th>
+              <th align="center" class="px-6 pt-6 pb-4">Tanker</th>
+              <th></th>
+              <th align="right">
+                <div class="text-center whitespace-nowrap text-left text-xs font-medium text-gray-700 uppercase">
+                  <button @click.prevent="addNewDetailForm()">
+                    <icon name="plus" class="w-4 h-4 mr-2 fill-green-600"/>
+                  </button>
+                </div>
+              </th>
+            </tr>
+          </thead>
+          <tbody class="bg-white divide-y divide-gray-200">
+            <tr v-for="(transaction, index) in form.transactions" :key="index" class="bg-gradient-to-r from-yellow-500 to-blue-600">
+              <td>
+                <div class="text-sm font-medium text-gray-900">
+                  <text-input v-model="transaction.trip_no" :error="errors.trip_no"/>
+                </div>
+              </td>
+              <td>
+                <div class="text-sm font-medium text-gray-900">
+                  <select :id="`driver-${index}`" v-model="transaction.driver_id" class="form-select" :class="{ error: errors[`driver_id.${index}`] }">
+                    <option :value="null" />
+                    <option v-for="driver in drivers" :key="driver.id" :value="driver.id">{{ driver.name }}</option>
+                  </select>
+                </div>
+              </td>
+              <td>
+                <div class="text-sm font-medium text-gray-900">
+                  <select :id="`helper-${index}`" v-model="transaction.helper_id" class="form-select" :class="{ error: errors[`helper_id.${index}`] }">
+                    <option :value="null" />
+                    <option v-for="helper in helpers" :key="helper.id" :value="helper.id">{{ helper.name }}</option>
+                  </select>
+                </div>
+              </td>
+              <td class="text-sm text-gray-500">
+                <div class="text-sm font-medium text-gray-900">
+                  <select :id="`tanker-${index}`" v-model="transaction.tanker_id" class="form-select" :class="{ error: errors[`tanker_id.${index}`] }">
+                    <option :value="null" />
+                    <option v-for="tanker in tankers" :key="tanker.id" :value="tanker.id">{{ tanker.plate_no }}</option>
+                  </select>
+                </div>
+              </td>
+              <td></td>
+              <td class="text-sm text-gray-500" align="right">
+                <div class=" px-5 text-sm font-medium text-gray-900">
+                <!-- <div class="px-5 py-4 whitespace-nowrap text-left text-xs font-medium text-gray-500 uppercase bg-white"> -->
+                  <button @click.prevent="deleteDetailForm(index)" type="button" class="py-1 px-1 flex-shrink-0 text-sm leading-none" tabindex="-1">
+                    <icon name="trash" class="w-4 h-4 mr-2 fill-red-600"/>
+                  </button>
+                </div>
+              </td>
+            </tr>
 
-              </tbody>
-            </table>
-          </div>
-          <div>
-            Driver list
-            <div v-for="driver in assignedDrivers" :key="driver">{{ driver }}</div>
-          </div>
-          <!-- /Details table input form -->
-        </div>
+          </tbody>
+        </table>
+      </div>
+      <!-- /Transactions table input form -->
 
-        <div class="px-8 py-4 bg-gray-100 border-t border-gray-200 flex justify-end items-center">
-          <loading-button :loading="sending" class="btn-indigo" type="submit">Save</loading-button>
-        </div>
-      </form>
-    </div>
+      <div class="px-8 py-4 bg-gray-100 border-t border-gray-200 flex justify-end items-center">
+        <loading-button :loading="sending" class="btn-indigo" type="submit">Save</loading-button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -218,26 +174,6 @@ export default {
   methods: {
     showDate (date) {
       this.form.date = date;
-    },
-
-    onChange (event) {
-      // this.driverSet.add(event.target.value);
-      // this.assignedDrivers = [...this.driverSet];
-      // const weee = this.assignedDrivers.map(driver => {
-      //   return driver + 'weee';
-      // });
-
-      // console.log(weee);
-      // const filteredArr = this.form.transactions.reduce((acc, current) => {
-      //   const x = acc.find(item => item.id === current.id);
-      //   if (!x) {
-      //     return acc.concat([current]);
-      //   } else {
-      //     return acc;
-      //   }
-      // }, []);
-
-      // console.log(filteredArr);
     },
 
     submit() {
