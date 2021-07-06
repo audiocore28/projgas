9375154f25311c49f180077d4a856d2d49a91ae9
jeffreybalmes <jefbalmes@gmail.com>
2021-07-06 14:50:20 +0800

    Auth - added guest middleware on login

--- routes/web.php
+++ routes/web.php
@@ -71,12 +71,12 @@ use App\Http\Controllers\PermissionController;
 
  // Auth
  Route::get('login', [AuthenticatedSessionController::class, 'create'])
-     ->name('login');
-     // ->middleware('guest');
+     ->name('login')
+     ->middleware('guest');
 
  Route::post('login', [AuthenticatedSessionController::class, 'store'])
-     ->name('login.store');
-     // ->middleware('guest');
+     ->name('login.store')
+     ->middleware('guest');
 
  Route::delete('logout', [AuthenticatedSessionController::class, 'destroy'])
      ->name('logout');
